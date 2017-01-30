7681f5bf2e2bb209ea5923bb8653d3dfb93ab0a4
Liyan David Chang <git@ldchang.com>
2017-01-29 21:46:17 -0500

    Patch for if there is a 404 inside the page

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 chrome.webRequest.onCompleted.addListener(function(details) {
-    if (details.statusCode == 404) {
-        //
-        var newUrl = new URL(details.url);
-        newUrl.host = "obamawhitehouse.archives.gov";
-        chrome.tabs.query({'active': true}, function(tabs) {
-          chrome.tabs.update(tabs[0].id, {url: newUrl.href});
-        });
-    }
-},
-{urls: ["*://*.whitehouse.gov/*"]},
-["responseHeaders"]
+        if (details.statusCode == 404) {
+            chrome.tabs.query({'active': true}, function(tabs) {
+
+                // Detect if this is the url in the tab. Avoid redirects when
+                // it's a 404 in the page
+                var currentUrl = tabs[0].url;
+                if (details.url == currentUrl) {
+                    // Rewrite URL
+                    var newUrl = new URL(details.url);
+                    newUrl.host = "obamawhitehouse.archives.gov";
+
+                    // Redirect
+                    chrome.tabs.update(tabs[0].id, {url: newUrl.href});
+                }
+            });
+        }
+    },
+    {urls: ["*://*.whitehouse.gov/*"]},
+    ["responseHeaders"]
 );
 
 
