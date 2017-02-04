99503cf23660e391f907a4b5e3377211928016f0
Robbie Holmes <robbiethegeek@gmail.com>
2017-02-03 21:44:59 -0500

    Check if HTTP status for new url is 200 on Obama archive.

--- src/main.js
+++ src/main.js
@@ -6,12 +6,16 @@ chrome.webRequest.onCompleted.addListener(function(details) {
                 // it's a 404 in the page
                 var currentUrl = tabs[0].url;
                 if (details.url == currentUrl) {
-                    // Rewrite URL
                     var newUrl = new URL(details.url);
                     newUrl.host = "obamawhitehouse.archives.gov";
-
-                    // Redirect
-                    chrome.tabs.update(tabs[0].id, {url: newUrl.href});
+                    var xhr = new XMLHttpRequest();
+                    // Had to use a CORS proxy - https://crossorigin.me/
+                    xhr.open("GET", `https://crossorigin.me/${newUrl.href}`, false);
+                    xhr.send()
+                    if (xhr.status == 200) {
+                        // Redirect since the page exists on the Obama archive.
+                        chrome.tabs.update(tabs[0].id, {url: newUrl.href});
+                    }
                 }
             });
         }
